d0687d983832254c554eab603008ef58c1e2ce31
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-09-22 22:21:51 +0200

    Remove tests

--- app/src/androidTest/java/me/devsaki/hentoid/ai_upscale/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/me/devsaki/hentoid/ai_upscale/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package me.devsaki.hentoid.ai_upscale
+package me.robb.ai_upscale
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("me.devsaki.hentoid.ai_upscale", appContext.packageName)
+        assertEquals("me.robb.ai_upscale", appContext.packageName)
     }
 }
